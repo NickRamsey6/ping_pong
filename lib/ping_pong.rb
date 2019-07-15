b08d61f9f503280c08fb3ee72785c449de8f7be5
Student <example@example.com>
2019-07-15 10:57:49 -0700

    refactor code to use the integer class

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,25 +1,26 @@
 #!/usr/bin/env ruby
-
-def ping_pong(input)
-  x = 0
-  output_array = []
-  while (x < input)
-    x = x + 1
-    if ((x % 3) === 0) & ((x % 5) === 0)
-      output_array.push("pingpong")
-    elsif (x % 3) === 0
-     output_array.push("ping")
-    elsif (x % 5) === 0
-     output_array.push("pong")
-    else
-      output_array.push(x)
+class Integer
+  def ping_pong
+    x = 0
+    output_array = []
+    while (x < self)
+      x = x + 1
+      if ((x % 3) === 0) & ((x % 5) === 0)
+        output_array.push("pingpong")
+      elsif (x % 3) === 0
+       output_array.push("ping")
+      elsif (x % 5) === 0
+       output_array.push("pong")
+      else
+        output_array.push(x)
+      end
     end
+    puts(output_array)
+    output_array
   end
-  puts(output_array)
-  output_array
 end
 
 puts 'Enter a Number'
 input = gets.to_i
 
-ping_pong(input)
+input.ping_pong
