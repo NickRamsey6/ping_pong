216cac6a10b04bd1ab47853ce2dc210bdb641334
Student <example@example.com>
2019-07-15 11:00:38 -0700

    update spec tests

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,27 +1,27 @@
 require('rspec')
 require('ping_pong')
 
-describe('#ping_pong') do
-  it("counts to an inputted number by 1") do
-    expect(ping_pong(8)).to(eq([1,2,3,4,5,6,7,8]))
-  end
-end
+# describe('#ping_pong') do
+#   it("counts to an inputted number by 1") do
+#     expect(8.ping_pong).to(eq([1,2,3,4,5,6,7,8]))
+#   end
+# end
 
 
 describe('#ping_pong') do
   it("returns ping for every number divisible by 3") do
-    expect(ping_pong(3)).to(eq([1,2,"ping"]))
+    expect(3.ping_pong).to(eq([1,2,"ping"]))
   end
 end
 
 describe('#ping_pong') do
   it("returns pong for every number divisible by 5") do
-    expect(ping_pong(5)).to(eq([1,2,"ping",4,"pong"]))
+    expect(5.ping_pong).to(eq([1,2,"ping",4,"pong"]))
   end
 end
 
 describe('#ping_pong') do
   it("returns pingpong for every number divisible by 15") do
-    expect(ping_pong(15)).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]))
+    expect(15.ping_pong).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]))
   end
 end
